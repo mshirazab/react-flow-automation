d963c88f2207f8fb1d9f13be8ee0c948cdf4c7b6
Mohamed Shiraz <shiraz@youplus.com>
2019-10-24 16:56:14 +0530

    feat(login): added Login form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './App.module.scss';
 import Header from './layout/Header';
+import Login from './components/Login';
 
 const App = () => (
   <div className={styles.layout}>
     <Header />
+    <content>
+      <Login />
+    </content>
   </div>
 );
 export default App;
---
+++ src/components/Login/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+const Login = () => (
+  <form className={styles.container}>
+    <p className={styles.heading}>Sign-In</p>
+    <input />
+  </form>
+);
+
+export default Login;
